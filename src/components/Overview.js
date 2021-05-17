8a57c4b6d75123545e0a881dc782dc425214854e
Enkh-Amar <enkhamar1220@gmail.com>
2021-05-17 18:26:13 +0800

    some changes

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -10,6 +10,7 @@ import MoreVertIcon from '@material-ui/icons/MoreVert';
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box'
 import PlayCircleFilledWhiteIcon from '@material-ui/icons/PlayCircleFilledWhite';
+import Divider from '@material-ui/core/Divider';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -17,7 +18,7 @@ const useStyles = makeStyles((theme) => ({
         flexGrow: 1,
     },
     paper: {
-        padding: theme.spacing(2),
+        padding: theme.spacing(0),
         margin: 'auto',
         marginTop: 30,
         marginBottom: 50,
@@ -49,6 +50,10 @@ const useStyles = makeStyles((theme) => ({
     }
 }));
 
+const smallAvatar = (<Typography style={{ margin: 8 }}>
+                        <Typography component="div" style={{ background: "rgb(94,53,177)", borderRadius: "50%", width: "50px", height: "50px" }} />
+                    </Typography>)
+
 
 function Overview() {
     const classes = useStyles();
@@ -60,7 +65,7 @@ function Overview() {
                     <Grid item zeroMinWidth>
                     <CssBaseline />
                     <Container maxWidth="sm">
-                        <Typography component="div" style={{ backgroundColor: 'rgb(178, 223, 219)', height: '20vh', width: '20vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }} >
+                        <Typography component="div" style={{ backgroundColor: 'rgb(178, 223, 219)', height: '20vh', width: '20vh', display: 'flex', justifyContent: 'center', alignItems: 'center', marginLeft: -24 }} >
                             <PlayCircleFilledWhiteIcon style={{ color: 'white', fontSize: 40, cursor: 'pointer' }} />
                         </Typography>
                     </Container>
@@ -69,17 +74,18 @@ function Overview() {
                         <Grid xs container direction="column" alignItems="flex-start" justify="space-around" spacing={2} >
 
                             <Grid item>
-                                <Typography variant="h4">
+                                <Typography variant="h4" style={{ color: 'rgb(97,97,97)' }}>
                                     Features
                                 </Typography>
                             </Grid>
                             <Grid container justify='flex-start'>
-                                <Typography variant="p">
+                                <Typography variant="p" style={{ color: 'rgb(97,97,97)' }}>
                                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sagittis pretium dolor, id tincidunt nunc.
                                 </Typography>
                             </Grid>
                             <Grid container justify='center'>
-                                <Typography variant="p" style={{ cursor: 'pointer', color: 'rgba(0,187,211)', fontWeight: 900, textAlign: 'center' }}>
+                                <Typography variant="p" style={{ cursor: 'pointer', color: 'rgb(0,187,211)', fontWeight: 900, textAlign: 'center' }}>
+                                    <Divider variant="fullWidth" />
                                     READ OUR FEATURES
                                 </Typography>
                             </Grid>
@@ -95,12 +101,26 @@ function Overview() {
             
             <Paper className={classes.paper} square>
                 <Grid container>
-                    
-                </Grid>
-                <Grid item>
-                    <IconButton aria-label="settings">
-                        <MoreVertIcon />
-                    </IconButton>
+                    <Grid item xs style={{ padding: 15 }}>
+                        <Grid container justify="flex-start">
+                            <Typography variant="h4" style={{ color: 'rgb(97,97,97)' }}>
+                                Details
+                            </Typography>
+                        </Grid>
+                        <Grid container direction="column">
+                            {smallAvatar}
+                            <Divider />
+                            {smallAvatar}
+                            {smallAvatar}
+                            {smallAvatar}
+                            {smallAvatar}
+                        </Grid>
+                    </Grid>
+                    <Grid >
+                        <IconButton aria-label="settings">
+                            <MoreVertIcon />
+                        </IconButton>
+                    </Grid>
                 </Grid>
             </Paper>
         </div>
